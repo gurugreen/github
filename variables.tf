88d3a65eb22b80403a2451ed32fdb82777837cd8
gurugreen <guruprasanna.green@gmail.com>
2024-12-30 19:34:16 +0530

    first commit

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region where the bucket will be created"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
